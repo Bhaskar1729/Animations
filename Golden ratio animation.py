d16f99dc055badd7a72df5287ad7ecbaff1fad42
Bhaskar1729 <67594598+Bhaskar1729@users.noreply.github.com>
2021-12-07 12:36:14 +0530

    Update Golden ratio animation.py

--- Golden ratio animation.py
+++ Golden ratio animation.py
@@ -12,17 +12,16 @@ label.place(x = 50, y = 50)
 time.sleep(2)
 while r < 1:
     bud = canvas.create_oval(395, 345, 405, 355, fill = "yellow")
-    rad = 10
+    rad = 10        
     label.config(text = str(r))
-    x = r*2*math.pi - (math.pi)/2
+    x = r*2*math.pi   #angle
 
-    for i in range(1, 200):
+    for i in range(1, 200):     #200 buds
         rad = rad + 0.75
-        pos = canvas.coords(bud)
         canvas.move(bud, rad*math.cos(x), rad*math.sin(x))
         pos = canvas.coords(bud)
         bud = canvas.create_oval(pos[0], pos[1], pos[2], pos[3], fill = "yellow")
-        x = x + r*2*math.pi             
+        x = x + r*2*math.pi                 
     tk.update()
     r += 0.0001
     time.sleep(0.001)
